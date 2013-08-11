c87042dbf64255be3b172376b54f84664863b6c1
Seh Hui, Leong <felixleong@gmail.com>
2013-08-12 07:45:10 +0800

    Added "endpoints" config to allow additional endpoints.

--- mdx_oembed/extension.py
+++ mdx_oembed/extension.py
@@ -11,6 +11,10 @@ AVAILABLE_ENDPOINTS = ENDPOINTS.keys()
 class OEmbedExtension(Extension):
 
     config = {
+        'endpoints': [
+            [],
+            "A list of additional oEmbed endpoints to support",
+        ],
         'allowed_endpoints': [
             AVAILABLE_ENDPOINTS,
             "A list of oEmbed endpoints to allow. Possible values are "
@@ -25,10 +29,19 @@ class OEmbedExtension(Extension):
         md.inlinePatterns.add('oembed_link', link_pattern, '<image_link')
 
     def prepare_oembed_consumer(self):
+        # Support for additional endpoints
+        additional_endpoints = {
+            key: oembed.OEmbedEndpoint(entry['endpoint'], entry['urlpatterns'])
+            for key, entry in self.getConfig('endpoints', []).items()
+        }
+        supported_endpoints = ENDPOINTS.copy()
+        supported_endpoints.update(additional_endpoints)
+
+        # Configure for supported endpoints
         allowed_endpoints = self.getConfig('allowed_endpoints',
-                                           AVAILABLE_ENDPOINTS)
+                                           supported_endpoints.keys())
         consumer = oembed.OEmbedConsumer()
         [consumer.addEndpoint(v)
-         for k,v in ENDPOINTS.items()
+         for k, v in supported_endpoints.items()
          if k in allowed_endpoints]
         return consumer
